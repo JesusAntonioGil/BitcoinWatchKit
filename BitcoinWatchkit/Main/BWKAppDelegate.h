08433b6ad1d7cbe7daebb6062dc1134d94011d3b
Jesus Antonio Gil <jesus.a22@hotmail.com>
2015-02-28 22:40:49 +0100

    First commit

---
+++ BitcoinWatchkit/Main/BWKAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BitcoinWatchkit
+//
+//  Created by Jesus Antonio Gil on 28/2/15.
+//  Copyright (c) 2015 Jesus Antonio Gil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface BWKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ BitcoinWatchkit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BitcoinWatchkit
+//
+//  Created by Jesus Antonio Gil on 28/2/15.
+//  Copyright (c) 2015 Jesus Antonio Gil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BWKAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BWKAppDelegate class]));
+    }
+}
